988c79b00d5741e0ed0ec4dbdf4d7ec8d09f6a17
danilogf91 <danilogrnd@gmail.com>
2024-06-22 23:29:19 -0500

    Meter view panel first test

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,22 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/meter', function () {
+    return Inertia::render('Meters');
+})->middleware(['auth', 'verified'])->name('meter');
+
+Route::get('/meter-inputs', function () {
+    return Inertia::render('MeterInputs');
+})->middleware(['auth', 'verified'])->name('meter-inputs');
+
+Route::get('/meter-edit', function () {
+    return Inertia::render('MeterEdit');
+})->middleware(['auth', 'verified'])->name('meter-edit');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
