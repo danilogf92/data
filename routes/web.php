f6d4d84daf5291909c4c23e402a5c333e29df5c6
danilogf92 <danilogrnd91@gmail.com>
2024-07-05 01:37:23 -0500

    Test Plants

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
-
 use App\Http\Controllers\API\MeterController;
 use App\Http\Controllers\API\PlantController;
 use App\Http\Controllers\API\PowerBiController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\MeasurementController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\EnsureStaticTokenIsValid;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -13,24 +13,20 @@ use Inertia\Inertia;
 Route::redirect('/', '/dashboard');
 
 Route::middleware(['auth'])->group(function () {
-  // Route::get('/dashboard', fn () => Inertia::render('Dashboard'))->name('dashboard');
-  Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-  Route::resource('measurement', MeasurementController::class);
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('measurement', MeasurementController::class);
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::prefix('api')->group(function () {
+        Route::get('/plants', [PlantController::class, 'index']);
+        Route::get('/meters', [MeterController::class, 'index']);
+    });
 });
 
-Route::middleware('auth')->group(function () {
-  Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-  Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-  Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-Route::middleware('auth')->prefix('api')->group(function () {
-    Route::get('/plants', [PlantController::class, 'index']);
-    Route::get('/meters', [MeterController::class, 'index']);
-    // Route::get('/power-bi', [PowerBiController::class, 'index']);
-});
-
-Route::prefix('api')->group(function () {
+Route::prefix('api')->middleware(EnsureStaticTokenIsValid::class)->group(function () {
     Route::get('/measurements', [PowerBiController::class, 'measurements']);
     Route::get('/meter', [PowerBiController::class, 'meters']);
     Route::get('/metertypes', [PowerBiController::class, 'metertypes']);
@@ -38,3 +34,4 @@ Route::prefix('api')->group(function () {
 });
 
 require __DIR__ . '/auth.php';
+
